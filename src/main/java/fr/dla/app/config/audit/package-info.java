628eef476ffcb036b53ee3052ad11c97473363d0
david lacoste <david.lacoste@transatel.com>
2020-09-28 00:29:04 +0200

    Initial version of dlapp generated by JHipster-6.10.1

---
+++ src/main/java/fr/dla/app/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package fr.dla.app.config.audit;
---
+++ src/test/java/fr/dla/app/cucumber/CucumberContextConfiguration.java
@@ -0,0 +1,20 @@
+package fr.dla.app.cucumber;
+
+import fr.dla.app.DlappApp;
+import io.cucumber.java.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration(classes = DlappApp.class)
+public class CucumberContextConfiguration {
+
+    @Before
+    public void setup_cucumber_spring_context() {
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+    }
+
+}
